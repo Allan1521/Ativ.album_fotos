e86006e3e867d1f96a2a5f411093551a325dc31e
Allan <allan1521lima@gmail.com>
2024-08-05 22:15:59 -0300

    Atividada Pronta

--- index.php
+++ index.php
@@ -1,40 +1,31 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Edu+AU+VIC+WA+NT+Hand:wght@400..700&family=Vollkorn:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
     <title>Galeria de Imagens</title>
-    <style>
-    .gallery {
-        display: flex;
-        flex-wrap: wrap;
-    }
 
-    .gallery img {
-        width:19%;
-        margin: 0.5%;
-        transition: transform 0.2s;
-    }
-
-    .gallery img:hover {
-        transform: scale(1.5);
-    }
-    </style>
 </head>
 
 <body>
     <h1>Galeria de Imagens</h1>
-    <form action="upload.php" method="post" enctype="mulitpart/form-data">
-        <input type="file" name = "image" accept="image/*">
-        <button type="submit"> Enviar Imagem</button>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*">
+        <button type="submit"> Enviar Imagem </button>
     </form>
     <div class="gallery">
-    <?php
-    $file = glob("uploads/*.*");
+        <?php
+    $files = glob("uploads/*.*");
     foreach($files as $file){
-        echo '<img src="' . $file . '" alt=Imagem">';   
+        echo '<img src="' . $file . '" alt="Imagem">';   
     }
     ?>
     </div>
--- upload.php
+++ upload.php
@@ -1,25 +1,40 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
-    if($_FILES['image']['error'] == UPOLOAD_ERR_OK){
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['image'])) {
+    if($_FILES['image']['error'] == UPLOAD_ERR_OK) {
         $target_dir = "uploads/";
-        $target_file = $target_dir . basename($_FILES["image"] ["name"]);
+        $target_file = $target_dir . basename($_FILES["image"]["name"]);
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        //Verificar se o arquivo é uma imagem real
-        $check = getimagesize($_FILES["image"]["name"]);
-        if ($check !== false){
-            if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-                echo "O arquivo" . basename($_FILES["image"]["name"]) . "foi envaido com sucesso.";
-            } else {
-                echo "desculpe, houve um erro ao enviar o seu arquivo.";
+ 
+        //verifica se o arquivo é uma imagem real
+        $check = getimagesize($_FILES["image"]["tmp_name"]);
+        if($check !== false){
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                echo "O arquivo ". basename($_FILES["image"]["name"]).
+                " foi enviado com sucesso.";
+            }else{
+                echo "Desculpe, houve um erro ao enviar o seu arquivo.";
             }
         } else {
-            echo "O arquivo não é uma imagem."
+            echo "O arquivo não é uma imagem";
         }
-            
-        } else {
-            //Mostrar mensagem de erro específica
-        }
+    }else{
+        //mostra mensagem de erro específica
+        echo "Erro ao upload: " . $_FILES['image']['error'];
     }
+ 
+    
+}else{
+    echo "Nenhum arquivo enviado";
 }
-?>
+?>  
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta http-equiv="refresh" content="2; url=index.php">
+    <title>Upload de imagem</title>
+</head>
+<body>
+    <p>Redirecionamento para a galeria</p>
+</body>
+</html>
